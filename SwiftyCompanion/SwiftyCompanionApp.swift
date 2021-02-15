02f96b57952225dcf0f79e624e3c00c3734a39f4
Dmytro Shults <dmytroshults@gmail.com>
2021-02-15 18:14:29 +0100

    Initial Commit

---
+++ SwiftyCompanion/SwiftyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCompanionApp.swift
+//  SwiftyCompanion
+//
+//  Created by Dmytro Shults on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
